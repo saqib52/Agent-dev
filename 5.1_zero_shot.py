ca098f54e2904f148cbd07a9f27702849cf6023d
Saqib52 <saqibjawad.ai@gmail.com>
2026-05-31 12:03:19 +0500

    llm-prompting

---
+++ 5.1_zero_shot.py
@@ -0,0 +1,10 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+load_dotenv()   
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=os.getenv("GEMINI_API_KEY"),
+    )
+
